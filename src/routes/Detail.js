0f7bca86a4e56229ec2327b56929399d7d8b92d3
Kevin Youngho Seo <keviny.seo@gmail.com>
2020-12-15 23:13:14 +0900

    #1.2

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,1 +1,27 @@
-export default () => "Deatil";
+import React from 'react'
+import {useParams} from "react-router-dom";
+import {gql, useQuery} from "@apollo/client";
+
+const GET_MOVIE = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+export default function Detail() {
+  const { id } = useParams();
+  const {loading, data} = useQuery(GET_MOVIE, {
+    variables: {id}
+  });
+  return (
+    <div>
+      {loading && <h1>Loading...</h1>}
+      {!loading && data && data.movie && <h1>{data.movie.title}</h1>}
+    </div>
+  )
+}
