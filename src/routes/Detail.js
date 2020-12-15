4ec91adaf24aff2aa7629f1f6c9df30fa1fe7584
Kevin Youngho Seo <keviny.seo@gmail.com>
2020-12-15 23:25:26 +0900

    #1.3

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,52 @@
 import React from 'react'
 import {useParams} from "react-router-dom";
 import {gql, useQuery} from "@apollo/client";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
     movie(id: $id) {
-      id
       title
       medium_cover_image
+      language
+      rating
       description_intro
     }
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
 `;
 
 export default function Detail() {
@@ -19,9 +55,13 @@ export default function Detail() {
     variables: {id}
   });
   return (
-    <div>
-      {loading && <h1>Loading...</h1>}
-      {!loading && data && data.movie && <h1>{data.movie.title}</h1>}
-    </div>
+    <Container>
+      <Column>
+        <Title>Name</Title>
+        <Subtitle>English - 4.5</Subtitle>
+        <Description>lorem ipsum lalala</Description>
+      </Column>
+      <Poster></Poster>
+    </Container>
   )
 }
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -47,6 +47,15 @@ const Loading = styled.div`
   margin-top: 10px;
 `;
 
+const Movies = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 25px;
+  width: 60%;
+  position: relative;
+  top: -50px;
+`;
+
 const Home = () => {
   const { loading, data } = useQuery(GET_MOVIES);
   return (
@@ -58,7 +67,7 @@ const Home = () => {
       {loading && <Loading> Loading... </Loading>}
       {!loading && 
         data.movies &&
-        data.movies.map(m => <Movie key={m.id} id={m.id} />)}
+        data.movies.map(m => <Movie key={m.id} id={m.id} bg={m.medium_cover_image} />)}
     </Container>
   )
 }
